12499aa232d206c24720f2eb5bd932bbc1822097
facubra <facu_jl@hotmail.com>
2022-11-11 22:41:18 -0300

    Funciones del objeto - car - movimiento y colision FacundoBrajeda

--- src/components/componentsPhaser/Nivel.js
+++ src/components/componentsPhaser/Nivel.js
@@ -8,15 +8,18 @@ export default class Nivel extends Phaser.Scene {
         this.platform = null;
         this.nenufar = null;
         this.x = 0;
-        this.lake=null;
-        this.meta=null;
+        this.lake = null;
+        this.meta = null;
+        this.car = null;
+        this.car2 = null;
+        this.car3 = null;
 
     }
     create() {
         this.physics.world.setBoundsCollision(true, true, true, true);
         this.lake = this.physics.add.staticGroup({
-            key:'lake',
-            setScale:{x:0.4,y:0.1}
+            key: 'lake',
+            setScale: { x: 0.4, y: 0.1 }
         })
         //this.lake=this.physics.add.image(320, 157, 'lake').setSize(300,300,true);
         this.playerWin = this.physics.add.staticGroup();
@@ -31,7 +34,7 @@ export default class Nivel extends Phaser.Scene {
             repeat: 4,
             setXY: { x: 55, y: 65, stepX: 171 },
             setScale: { x: 1.5, y: 1.5 },
-            immovable:true
+            immovable: true
         });
         this.nenufar.refresh();
         this.cursors = this.input.keyboard.createCursorKeys();
@@ -42,7 +45,7 @@ export default class Nivel extends Phaser.Scene {
             setScale: { x: 2, y: 2.8 },
         });
 
-        this.meta=this.physics.add.staticGroup();
+        this.meta = this.physics.add.staticGroup();
         this.meta.create(399, 53, 'meta').setScale(2).refreshBody();
         this.player = this.physics.add.sprite(330, 668, 'frog').setScale(1.5);
         this.player.setCollideWorldBounds(true);
@@ -67,7 +70,7 @@ export default class Nivel extends Phaser.Scene {
         });
         this.anims.create({
             key: 'down',
-            frames: this.anims.generateFrameNumbers('frog', { frames: [6,7,6] }),
+            frames: this.anims.generateFrameNumbers('frog', { frames: [6, 7, 6] }),
             frameRate: 10
         });
         this.anims.create({
@@ -77,27 +80,76 @@ export default class Nivel extends Phaser.Scene {
         });
         this.physics.add.overlap(this.player, this.nenufar, this.win, null, this);
         //this.physics.add.overlap(this.player, this.lake, this.dead, null, this);
+
+        this.createCar();
+
+
+
     }
 
+    createCar() {
+        this.car = this.physics.add.group();
+        this.car2 = this.physics.add.group();
+        let distCar = 670;
+        let distCar2 = 0;
+    
+        for (var i = 0; i < 2; i++) {
+
+            this.car.create(distCar, 400, 'car1').setScale(1.5);
+            this.car2.create(distCar2, 450, 'car2').setScale(1.5);
+            this.car.create(distCar, 500, 'car3').setScale(1.5);
+            distCar += 200;
+            distCar2 +=200;
+        }
+
+
+        this.physics.add.overlap(this.player, this.car, this.chocarPlayer, null, this);
+        this.physics.add.overlap(this.player, this.car2, this.chocarPlayer, null, this);
+
+    }
+
+    moveCar() {
+        this.car.setVelocityX(-100);
+        this.car2.setVelocityX(100);
+        this.resetPositionCar();
+    }
+
+    resetPositionCar() {
+        if (this.car.x <= 0) {
+            this.car.x = 675;
+        }
+    }
+
+    chocarPlayer(player, car) {
+        this.player.destroy();
+        this.player = this.physics.add.sprite(330, 668, 'frog').setScale(1.5);
+        this.physics.add.overlap(this.player, this.car, this.chocarPlayer, null, this);
+    }
+
+
+
     update() {
+
+        this.moveCar()
+
         //console.log(this.player.y, this.player.x)
         //  Horizontal movement every 250ms
         if (this.input.keyboard.checkDown(this.cursors.left, 250)) {
-            this.player.x -=30;
+            this.player.x -= 30;
             this.player.anims.play('left', true)
         }
-        else if (this.input.keyboard.checkDown(this.cursors.right, 250) ) {
-            this.player.x +=30;
+        else if (this.input.keyboard.checkDown(this.cursors.right, 250)) {
+            this.player.x += 30;
             this.player.anims.play('right', true)
         }
 
         //  Vertical movement every 150ms
-        if (this.input.keyboard.checkDown(this.cursors.up, 150) ) {
-            this.player.y -=30;
+        if (this.input.keyboard.checkDown(this.cursors.up, 150)) {
+            this.player.y -= 30;
             this.player.anims.play('up', true)
         }
-        else if (this.input.keyboard.checkDown(this.cursors.down, 150) ) {
-            this.player.y +=30;
+        else if (this.input.keyboard.checkDown(this.cursors.down, 150)) {
+            this.player.y += 30;
             this.player.anims.play('down', true)
         }
         // else{
@@ -113,19 +165,20 @@ export default class Nivel extends Phaser.Scene {
             }
         }
     }
-    win(player ,nenufar) {
+    win(player, nenufar) {
         //this.playerWin.create(55, 65, 'frogWin').setScale(1.5);
         this.nenufar.killAndHide(nenufar);
-        nenufar.body.enable=false;
-        this.player.destroy();   
+        nenufar.body.enable = false;
+        this.player.destroy();
         this.player = this.physics.add.sprite(330, 668, 'frog').setScale(1.5);
         this.player.setCollideWorldBounds(true);
         this.physics.add.overlap(this.player, this.nenufar, this.win, null, this);
     }
-    dead(player ,nenufar) {
-        this.player.destroy();   
+    dead(player, nenufar) {
+        this.player.destroy();
         this.player = this.physics.add.sprite(330, 668, 'frog').setScale(1.5);
         this.player.setCollideWorldBounds(true);
         this.physics.add.overlap(this.player, this.lake, this.dead, null, this);
     }
+
 };
--- src/components/componentsPhaser/Preload.js
+++ src/components/componentsPhaser/Preload.js
@@ -10,6 +10,9 @@ export default class Preload extends Phaser.Scene {
         this.load.image('platform', 'assets/assetsPhaser/plataform.png');
         this.load.spritesheet('frog', 'assets/assetsPhaser/frogger.png', { frameWidth: 31.25, frameHeight: 28.5});
         this.load.image('frogWin', 'assets/assetsPhaser/froggerWin.png');
+        this.load.image('car1', 'assets/assetsPhaser/car1.png');
+        this.load.image('car2', 'assets/assetsPhaser/car2.png');
+        this.load.image('car3', 'assets/assetsPhaser/car3.png');
     }
 
     create() {
